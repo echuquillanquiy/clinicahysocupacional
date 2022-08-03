a0af39bf36d8f5dfefbea3d5af69bc4421b464ec
echuquillanquiy <edward.axl3@gmail.com>
2022-08-03 11:21:43 -0500

    quitando cmapos a mostrar jobs

--- resources/views/webPage/includes/works.blade.php
+++ resources/views/webPage/includes/works.blade.php
@@ -29,17 +29,17 @@
                             <div class="info">
                                 <div class="meta">
                                     <ul>
-                                        {{--<li><a href="{{ route('trabajos.index') }}">{{ $job->area->name }}</a></li>
-                                        <li>{{ $job->start }} - {{ $job->place->name }} - {{ $job->schedule->name }}</li>--}}
+                                        <li><a href="{{ route('trabajos.index') }}">{{ $job->area->name }}</a></li>
+                                        <li>{{ $job->start }} - {{ $job->place->name }} - {{ $job->schedule->name }}</li>
                                         <li><span class="text-primary"><i class="fas fa-users"></i>  Postulantes: </span> {{ $job->applicants_count }}</li>
                                     </ul>
                                 </div>
-                                <h4>
+                                {{--<h4>
                                     <a href="#">{{ $job->title }}</a>
                                 </h4>
                                 <p>
                                     {!! Str::limit($job->description, 40) !!}
-                                </p>
+                                </p>--}}
                             </div>
                         </div>
                     </div>
