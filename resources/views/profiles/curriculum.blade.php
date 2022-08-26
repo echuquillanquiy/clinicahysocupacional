39da5b29147b75d3bcb9a29980e1ba71e551c8d4
echuquillanquiy <edward.axl3@gmail.com>
2022-08-26 12:16:22 -0500

    perfil de postulantes

--- resources/views/profiles/curriculum.blade.php
+++ resources/views/profiles/curriculum.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="container py-4">
-        {{--<div class="card">
+        <div class="card">
             @isset($user->profile_applicant->user_id)
                 <div class="card-header text-center p-4">
                     <h1 class="text-gray-500 text-4xl font-bold">DATOS PERSONALES</h1>
@@ -112,9 +112,7 @@
                     </div>
                 </form>
             @endisset
-        </div>--}}
-
-        {{ $user->profile_applicant }}
+        </div>
     </div>
     <div class="container">
         <div class="card py-2 text-center text-red-600 font-black">
